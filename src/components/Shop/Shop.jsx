2a6ef510d0167f2d52317d0dc451c6d9ff141123
Shihan Chowdhury <hackedgdown@gmail.com>
2023-03-29 15:12:45 +0100

    retreive data from localStorage(localCart) and set cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -17,11 +17,35 @@ const Shop = () => {
     }
     , []);
 
-    useEffect( () => {
-        const localCart =  getShoppingCart();
-        console.log(localCart);
+    useEffect(() => {
+        // console.log(products);
+        const localCart = getShoppingCart();
+        // console.log(localCart);
+        const savedCart = [];
+
+        // Step 1: Find the id form local Storage
+        for (const id in localCart) {
+            // Step 2: Find particular product from products [] / state using id
+            const addedProduct = products.find(product => product.id === id);
+
+            // Step 3: Set quantity to the addedProduct []
+            if (addedProduct) {
+                const quantity = localCart[id];
+                addedProduct.quantity = quantity;
+
+                // Step 4: add the addedProduct to our savedCart Array
+                savedCart.push(addedProduct);
+            }
+
+            // Step 5: Finally, Change the cart state 
+            setCart(savedCart);
+
+            console.log('added product, ', addedProduct);
+
+        }
+
     }
-    , []);
+    , [products]);
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
@@ -41,7 +65,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cartContainer">
-                <Cart cart = {cart}/>
+                <Cart cart={cart} />
             </div>
         </div>
     );
